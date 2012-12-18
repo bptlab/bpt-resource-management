4866af4618f271a4bd68979dda96edb713842476
ulm.benjamin <ulm.benjamin@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2012-12-18 13:58:59 +0000

    first steps of creating the interface

---
+++ bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/BPTSearchComponent.java
@@ -0,0 +1,12 @@
+package de.uni_potsdam.hpi.bpt.resource_management.vaadin;
+
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.VerticalLayout;
+
+public class BPTSearchComponent extends CustomComponent{
+	
+	public BPTSearchComponent() {
+		VerticalLayout layout = new VerticalLayout();
+		setCompositionRoot(layout);
+	}
+}
---
+++ bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/BPTSidebar.java
@@ -0,0 +1,26 @@
+package de.uni_potsdam.hpi.bpt.resource_management.vaadin;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+public class BPTSidebar extends CustomComponent{
+	
+	private VerticalLayout layout;
+	public BPTSidebar() {
+		layout = new VerticalLayout();
+		layout.setWidth("100%");
+		layout.setHeight("100%");
+		setCompositionRoot(layout);
+		BPTLoginComponent loginComponent = new BPTLoginComponent();
+		BPTSearchComponent searchComponent = new BPTSearchComponent();
+		layout.addComponent(searchComponent);
+		layout.addComponent(loginComponent);
+		
+		
+	}
+	
+	
+
+}
