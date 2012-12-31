0cf9dce319b147d4dad3ec7191b7d6f7f94e7073
livetsunni@gmail.com <livetsunni@gmail.com@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2012-12-31 00:22:50 +0000

    * set up the connector to CouchDB via Ektorp * mapped the data schema to a Java class * example document will be created/modified in CouchDB by running BPTDatabase class

---
+++ trunk/bpt-resource-management-common/src/main/java/de/uni_potsdam/hpi/bpt/resource_management/ektorp/BPTTool.java
@@ -0,0 +1,175 @@
+package de.uni_potsdam.hpi.bpt.resource_management.ektorp;
+
+import java.util.Date;
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class BPTTool {
+	
+	@JsonProperty("_id")
+	private String name;
+	@JsonProperty("_rev")
+	private String revision; // CouchDB-specific, never shown on website
+
+	private String description; // shall be rich text
+	private String provider;
+	@JsonProperty("download_url")
+	private String downloadURL; // URL to be checked if valid
+	@JsonProperty("documentation_url")
+	private String documentationURL; // URL to be checked if valid
+	@JsonProperty("screencast_url")
+	private String screencastURL; // URL to be checked if valid
+	private List<String> availability; 
+		/*
+		 * open source, freeware, shareware, free for academics, commercial, ...
+		 */
+	@JsonProperty("model_types")
+	private List<String> modelTypes; 
+		/*
+		 * BPMN, EPC, Petri net, UML Activity Diagram, Workflow Net, YAWL, BPEL, ...
+		 */
+	private List<String> platforms;
+		/*
+		 * Windows, Linux, Mac OSX, Android, iOS, ...
+		 */
+	@JsonProperty("supported_functionalities")
+	private List<String> supportedFunctionalities;
+		/* 
+		 * graphical model editor, model repository, verification of model properties, 
+		 * enactment, runtime adaptation, process discovery based on event data, 
+		 * conformance checking based on event data
+		 */
+	@JsonProperty("contact_name")
+	private String contactName;
+	@JsonProperty("contact_mail")
+	private String contactMail; // email address
+	@JsonProperty("date_created")
+	private Date dateCreated;
+	@JsonProperty("last_update")
+	private Date lastUpdate;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getRevision() {
+		return revision;
+	}
+
+	public void setRevision(String revision) {
+		this.revision = revision;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getProvider() {
+		return provider;
+	}
+	
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+	
+	public String getDownloadURL() {
+		return downloadURL;
+	}
+	
+	public void setDownloadURL(String downloadURL) {
+		if (BPTValidator.isValidURL(downloadURL))
+			this.downloadURL = downloadURL;
+	}
+	
+	public String getDocumentationURL() {
+		return documentationURL;
+	}
+	
+	public void setDocumentationURL(String documentationURL) {
+		if (BPTValidator.isValidURL(documentationURL))
+			this.documentationURL = documentationURL;
+	}
+	
+	public String getScreencastURL() {
+		return screencastURL;
+	}
+	
+	public void setScreencastURL(String screencastURL) {
+		if (BPTValidator.isValidURL(screencastURL))
+			this.screencastURL = screencastURL;
+	}
+	
+	public List<String> getAvailability() {
+		return availability;
+	}
+	
+	public void setAvailability(List<String> availability) {
+		this.availability = availability;
+	}
+	
+	public List<String> getModelTypes() {
+		return modelTypes;
+	}
+	
+	public void setModelTypes(List<String> modelTypes) {
+		this.modelTypes = modelTypes;
+	}
+	
+	public List<String> getPlatforms() {
+		return platforms;
+	}
+	
+	public void setPlatforms(List<String> platforms) {
+		this.platforms = platforms;
+	}
+	
+	public List<String> getSupportedFunctionalities() {
+		return supportedFunctionalities;
+	}
+	
+	public void setSupportedFunctionalities(List<String> supportedFunctionalities) {
+		this.supportedFunctionalities = supportedFunctionalities;
+	}
+	
+	public String getContactName() {
+		return contactName;
+	}
+	
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	
+	public String getContactMail() {
+		return contactMail;
+	}
+	
+	public void setContactMail(String contactMail) {
+		if (BPTValidator.isValidEmail(contactMail))
+			this.contactMail = contactMail;
+	}
+	
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+	
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+}
