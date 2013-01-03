9fafa7148002402c333aa372af080553c1c420df
livetsunni <livetsunni@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2013-01-03 22:23:40 +0000

    modified test and document intialisation of database

--- trunk/bpt-resource-management-common/src/test/java/de/uni_potsdam/hpi/bpt/resource_management/ektorp/test/BPTDatabaseTest.java
+++ trunk/bpt-resource-management-common/src/test/java/de/uni_potsdam/hpi/bpt/resource_management/ektorp/test/BPTDatabaseTest.java
@@ -72,9 +72,7 @@ public class BPTDatabaseTest {
 		exampleTool.setName("ProM");
 		exampleTool.setDescription("<b>ProM</b> is an <b>extensible</b> framework that supports a wide variety of process mining techniques in the form of plug-ins. It is <u>platform independent</u> as it is implemented in Java, and can be downloaded <u>free of charge</u>.");
 		exampleTool.setProvider("Eindhoven University of Technology");
-		exampleTool.setDownloadURL("");
 		exampleTool.setDocumentationURL("http://www.promtools.org/prom6/pack-docs.html");
-		exampleTool.setScreencastURL("");
 		exampleTool.setAvailabilities(new HashSet<String>(Arrays.asList("open source", "freeware")));
 		exampleTool.setModelTypes(new HashSet<String>(Arrays.asList("BPMN", "EPC", "Petri Net")));
 		exampleTool.setPlatforms(new HashSet<String>(Arrays.asList("Windows", "Linux", "Mac OSX")));
@@ -92,7 +90,6 @@ public class BPTDatabaseTest {
 		exampleTool.setDescription("<b>Activiti</b> is an open-source workflow engine written in Java that can execute business processes described in BPMN 2.0.");
 		exampleTool.setProvider("Alfresco");
 		exampleTool.setDownloadURL("http://www.activiti.org/download.html");
-		exampleTool.setDocumentationURL("");
 		exampleTool.setScreencastURL("http://www.activiti.org/screenshots.html");
 		exampleTool.setAvailabilities(new HashSet<String>(Arrays.asList("open source")));
 		exampleTool.setModelTypes(new HashSet<String>(Arrays.asList("BPMN")));
@@ -107,11 +104,8 @@ public class BPTDatabaseTest {
 	private BPTTool generateExampleToolThree() {
 		BPTTool exampleTool = new BPTTool();
 		exampleTool.setName("Signavio Process Editor");
-		exampleTool.setDescription("");
 		exampleTool.setProvider("Signavio GmbH");
-		exampleTool.setDownloadURL("");
 		exampleTool.setDocumentationURL("http://www.signavio.com/");
-		exampleTool.setScreencastURL("");
 		exampleTool.setAvailabilities(new HashSet<String>(Arrays.asList("free for academics", "commercial")));
 		exampleTool.setModelTypes(new HashSet<String>(Arrays.asList("BPMN", "EPC", "Petri Net", "UML Activity Diagram", "Workflow Net")));
 		exampleTool.setPlatforms(new HashSet<String>());
@@ -127,10 +121,8 @@ public class BPTDatabaseTest {
 		BPTTool exampleTool = new BPTTool();
 		exampleTool.setName("Yaoqiang BPMN Editor");
 		exampleTool.setDescription("Yaoqiang BPMN Editor is a graphical editor for business process diagrams, compliant with OMG specifications (BPMN 2.0).");
-		exampleTool.setProvider("");
 		exampleTool.setDownloadURL("http://sourceforge.net/projects/bpmn/");
 		exampleTool.setDocumentationURL("http://sourceforge.net/projects/bpmn/");
-		exampleTool.setScreencastURL("");
 		exampleTool.setAvailabilities(new HashSet<String>(Arrays.asList("open source")));
 		exampleTool.setModelTypes(new HashSet<String>(Arrays.asList("BPMN")));
 		exampleTool.setPlatforms(new HashSet<String>(Arrays.asList("Windows")));
@@ -146,16 +138,12 @@ public class BPTDatabaseTest {
 		BPTTool exampleTool = new BPTTool();
 		exampleTool.setName("WebSphere Business Modeler Advanced");
 		exampleTool.setDescription("<b>IBM WebSphere(R) Business Modeler Advanced Version 7 is IBM's premier advanced business process modeling and analysis tool for business users.It offers process modeling, simulation, and analysis capabilities to help business users understand, document, and deploy business processes for continuous improvement.</b><br><br><ul><li>Enables business users to design, model, and deploy vital business processes</li><li>Allows users to make informed decisions before deployment through advanced simulation capabilities based on modeled and actual data</li><li>Provides integrated industry content to help business users jumpstart solution development</li><li>Accelerates process optimization by allowing users to visualize and identify bottlenecks and inefficiencies in processes</li><li>Provides enhanced integration with the IBM BPM Suite and WebSphere Dynamic Process Edition through role-based business spaces, a unified end user interface that integrates BPM content for a holistic management of business processes</li><li>Enables subject matter experts to share models and collaborate to translate business intent into process models using a Web browser with WebSphere Business Compass</li></ul>");
-		exampleTool.setProvider("");
-		exampleTool.setDownloadURL("");
 		exampleTool.setDocumentationURL("http://www-01.ibm.com/software/integration/wbimodeler/advanced/");
-		exampleTool.setScreencastURL("");
 		exampleTool.setAvailabilities(new HashSet<String>(Arrays.asList("commercial")));
 		exampleTool.setModelTypes(new HashSet<String>(Arrays.asList("BPMN")));
 		exampleTool.setPlatforms(new HashSet<String>(Arrays.asList("Windows")));
 		exampleTool.setSupportedFunctionalities(new HashSet<String>(Arrays.asList("graphical model editor", "model repository")));
 		exampleTool.setContactName("IBM");
-		exampleTool.setContactMail("");
 		exampleTool.setDateCreated(new Date());
 		exampleTool.setLastUpdate(new Date());
 		return exampleTool;
