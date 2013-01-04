d0a68d9c91ae28cc3fa8f7d31afc1c336363a076
livetsunni <livetsunni@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2013-01-04 01:11:22 +0000

    clickable links in Vaadin table

--- bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/BPTContainerProvider.java
+++ bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/BPTContainerProvider.java
@@ -6,6 +6,9 @@ import org.ektorp.CouchDbConnector;
 
 import com.vaadin.data.Item;
 import com.vaadin.data.util.IndexedContainer;
+import com.vaadin.terminal.ExternalResource;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Link;
 
 import de.uni_potsdam.hpi.bpt.resource_management.ektorp.*;
 
@@ -28,40 +31,14 @@ public class BPTContainerProvider {
 		
 		/* part for real data - BEGIN */
 		
-		container.addContainerProperty("Name", String.class, null);
-		container.addContainerProperty("Description", String.class, null);
-		container.addContainerProperty("Provider", String.class, null);
-		container.addContainerProperty("Download", String.class, null);
-		container.addContainerProperty("Documentation", String.class, null);
-		container.addContainerProperty("Screencast", String.class, null);
-		container.addContainerProperty("Availability", String.class, null);
-		container.addContainerProperty("Model type", String.class, null);
-		container.addContainerProperty("Platform", String.class, null);
-		container.addContainerProperty("Supported functionality", String.class, null);
-		container.addContainerProperty("Contact name", String.class, null);
-		container.addContainerProperty("Contact mail", String.class, null);
-		container.addContainerProperty("Date created", Date.class, null);
-		container.addContainerProperty("Last update", Date.class, null);
+		addContainerProperties(container);
 		
 		List<BPTTool> tools = repository.getAll();
 		
 		for (int i = 0; i < tools.size(); i++) {
 			BPTTool tool = tools.get(i);
 			Item item = container.addItem(i);
-			item.getItemProperty("Name").setValue(tool.getName());
-			item.getItemProperty("Description").setValue(tool.getDescription());
-			item.getItemProperty("Provider").setValue(tool.getProvider());
-			item.getItemProperty("Download").setValue(tool.getDownloadURL());
-			item.getItemProperty("Documentation").setValue(tool.getDocumentationURL());
-			item.getItemProperty("Screencast").setValue(tool.getScreencastURL());
-			item.getItemProperty("Availability").setValue(asFormattedString(tool.getAvailabilities()));
-			item.getItemProperty("Model type").setValue(asFormattedString(tool.getModelTypes()));
-			item.getItemProperty("Platform").setValue(asFormattedString(tool.getPlatforms()));
-			item.getItemProperty("Supported functionality").setValue(asFormattedString(tool.getSupportedFunctionalities()));
-			item.getItemProperty("Contact name").setValue(tool.getContactName());
-			item.getItemProperty("Contact mail").setValue(tool.getContactMail());
-			item.getItemProperty("Date created").setValue(tool.getDateCreated());
-			item.getItemProperty("Last update").setValue(tool.getLastUpdate());
+			setItemPropertyValues(item, tool);
 		}
 		
 		/* part for real data - END */
@@ -115,7 +92,49 @@ public class BPTContainerProvider {
 		return uniqueValues;
 	}
 	
+	private static void addContainerProperties(IndexedContainer container) {
+		container.addContainerProperty("Name", String.class, null);
+		container.addContainerProperty("Description", String.class, null);
+		container.addContainerProperty("Provider", String.class, null);
+		container.addContainerProperty("Download", Component.class, null);
+		container.addContainerProperty("Documentation", Component.class, null);
+		container.addContainerProperty("Screencast", Component.class, null);
+		container.addContainerProperty("Availability", String.class, null);
+		container.addContainerProperty("Model type", String.class, null);
+		container.addContainerProperty("Platform", String.class, null);
+		container.addContainerProperty("Supported functionality", String.class, null);
+		container.addContainerProperty("Contact name", String.class, null);
+		container.addContainerProperty("Contact mail", Component.class, null);
+		container.addContainerProperty("Date created", Date.class, null);
+		container.addContainerProperty("Last update", Date.class, null);
+	}
+	
+	private static void setItemPropertyValues(Item item, BPTTool tool){
+		item.getItemProperty("Name").setValue(tool.getName());
+		item.getItemProperty("Description").setValue(tool.getDescription());
+		item.getItemProperty("Provider").setValue(tool.getProvider());
+		item.getItemProperty("Download").setValue(asLink(tool.getDownloadURL()));
+		item.getItemProperty("Documentation").setValue(asLink(tool.getDocumentationURL()));
+		item.getItemProperty("Screencast").setValue(asLink(tool.getScreencastURL()));
+		item.getItemProperty("Availability").setValue(asFormattedString(tool.getAvailabilities()));
+		item.getItemProperty("Model type").setValue(asFormattedString(tool.getModelTypes()));
+		item.getItemProperty("Platform").setValue(asFormattedString(tool.getPlatforms()));
+		item.getItemProperty("Supported functionality").setValue(asFormattedString(tool.getSupportedFunctionalities()));
+		item.getItemProperty("Contact name").setValue(tool.getContactName());
+		item.getItemProperty("Contact mail").setValue(asEmailLink(tool.getContactMail()));
+		item.getItemProperty("Date created").setValue(tool.getDateCreated());
+		item.getItemProperty("Last update").setValue(tool.getLastUpdate());
+	}
+	
 	private static String asFormattedString(Set<String> stringSet) {
 		return stringSet.toString().replace("[", "").replace("]", "");
+	}
+	
+	private static Link asLink(String linkString) {
+		return new Link(linkString, new ExternalResource(linkString));
+	}
+	
+	private static Link asEmailLink(String linkString) {
+		return new Link(linkString, new ExternalResource("mailto:" + linkString));
 	}
 }
