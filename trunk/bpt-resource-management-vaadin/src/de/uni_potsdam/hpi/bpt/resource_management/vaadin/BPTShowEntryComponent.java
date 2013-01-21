8b1e2c170614b2f04d54681874f0f509f6ee1ede
ulm.benjamin@gmail.com <ulm.benjamin@gmail.com@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2013-01-21 14:35:48 +0000

    Started to build Superclass for BPTTable

---
+++ trunk/bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/BPTShowEntryComponent.java
@@ -0,0 +1,14 @@
+package de.uni_potsdam.hpi.bpt.resource_management.vaadin;
+
+import java.util.ArrayList;
+import com.vaadin.ui.CustomComponent;
+
+public class BPTShowEntryComponent extends CustomComponent{
+	
+	public void showEntries(ArrayList<String> tagValues) {
+		
+	}
+
+}
+
+//TODO: implement Super-Class for BPTTable
