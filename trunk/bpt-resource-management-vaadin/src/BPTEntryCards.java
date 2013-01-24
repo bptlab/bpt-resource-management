66cb2cbfbac0fdf75037ca4069fe55bf7273d635
ulm.benjamin@gmail.com <ulm.benjamin@gmail.com@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2013-01-24 20:11:46 +0000

    Zu zeigende Einträge werden nur noch in Common gefiltert, Superklasse für BPTTable und neue Komponente BPTEntryCards angelegt

---
+++ trunk/bpt-resource-management-vaadin/src/BPTEntryCards.java
@@ -0,0 +1,14 @@
+import com.vaadin.data.util.IndexedContainer;
+
+import de.uni_potsdam.hpi.bpt.resource_management.vaadin.BPTShowEntryComponent;
+
+
+public class BPTEntryCards extends BPTShowEntryComponent{
+
+	@Override
+	protected void show(IndexedContainer tableEntries) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
