d963d1c9564041f683a5ed45f94f5cf67f7abf8a
ulm.benjamin@gmail.com <ulm.benjamin@gmail.com@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2013-02-11 23:06:42 +0000

    der "more"-Button funktioniert jetzt (per javaScript)

--- trunk/bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/BPTEntry.java
+++ trunk/bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/BPTEntry.java
@@ -5,19 +5,56 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Link;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.themes.BaseTheme;
 
 public class BPTEntry extends CustomLayout{
 	
 	private CustomLayout layout;
+	private String entryId;
+	private BPTEntry entry;
 	
 	public BPTEntry(Item item) {
 		super("entry");
-		this.setDebugId(item.getItemProperty("ID").getValue().toString());
+		entry = this;
+		entryId = item.getItemProperty("ID").getValue().toString();
+		this.setDebugId(entryId);
 		Button more = new Button("more");
+		more.addListener(new Button.ClickListener(){
+			public void buttonClick(ClickEvent event) {
+				getWindow().executeJavaScript(getJavaSrciptStringShow());
+				entry.setHeight("");
+			}
+
+			private String getJavaSrciptStringShow() {
+				String js = 
+		        "var nodes = document.getElementById('" + entryId +"').childNodes[0].childNodes;" +
+				"for(i=0; i<nodes.length; i+=1){" +
+					"if(nodes[i].className == 'extension'){" +
+						"nodes[i].style.display = 'block';" +
+					"}}";
+				return js;
+			}});
+		
 		more.setStyleName(BaseTheme.BUTTON_LINK);
 		this.addComponent(more, "button more");
 		Button less = new Button("less");
+		less.addListener(new Button.ClickListener(){
+			public void buttonClick(ClickEvent event) {
+				getWindow().executeJavaScript(getJavaSrciptStringHide());
+				entry.setHeight("");
+			}
+
+			private String getJavaSrciptStringHide() {
+				String js = 
+		        "var nodes = document.getElementById('" + entryId +"').childNodes[0].childNodes;" +
+				"for(i=0; i<nodes.length; i+=1){" +
+					"if(nodes[i].className == 'extension'){" +
+						"nodes[i].style.display = 'none';" +
+					"}}";
+				return js;
+				
+			}});
 		more.setStyleName(BaseTheme.BUTTON_LINK);
 		this.addComponent(less, "button less");
 		for(Object id : item.getItemPropertyIds()){
