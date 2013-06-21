9011e2ecd53b0f5786e7b1465ca1420062d360bd
livetsunni <livetsunni@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2013-06-21 21:08:41 +0000

    * sorting of entries is case-insensitive * fixed error that caused duplicate delivery of emails for entry update (when published or rejected)

--- trunk/bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/BPTEntryCards.java
+++ trunk/bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/BPTEntryCards.java
@@ -1,13 +1,17 @@
 package de.uni_potsdam.hpi.bpt.resource_management.vaadin;
 
+import java.io.Serializable;
+import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 import org.apache.tools.ant.taskdefs.Sleep;
 
 import com.vaadin.data.Item;
 import com.vaadin.data.Property;
 import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.util.DefaultItemSorter;
 import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.HorizontalLayout;
@@ -62,15 +66,15 @@ public class BPTEntryCards extends BPTShowEntryComponent {
 	@Override
 	protected void show(IndexedContainer entries) {
 		vertical.removeAllComponents();
-		for(BPTEntry entry : entryList){
+		for (BPTEntry entry : entryList) {
 			entry.removeAllComponents();
 		}
 		entryList.clear();
-		if(entries.size() == 0){
+		if (entries.size() == 0) {
 			return;
 		}
 		IndexedContainer sortedEntries = sortEntries(entries);
-		for(Object id : entries.getItemIds()){
+		for (Object id : sortedEntries.getItemIds()) {
 			Item item = entries.getItem(id);
 			System.out.println(item.getItemPropertyIds());
 			
@@ -78,8 +82,8 @@ public class BPTEntryCards extends BPTShowEntryComponent {
 			vertical.addComponent(entry);
 			entryList.add(entry);
 			
-			if(!isInitial){
-				for(int i = 0; i < entryList.size(); i++){
+			if (!isInitial) {
+				for (int i = 0; i < entryList.size(); i++) {
 					entryList.get(i).hideJavaScript();
 				}
 			}
@@ -92,6 +96,7 @@ public class BPTEntryCards extends BPTShowEntryComponent {
 		String sort = (String) sortSelect.getValue();
 		Object[] propertyId = {sort};
 		boolean[] ascending = {sort=="Name" || sort=="Provider"};
+		entries.setItemSorter(new CaseInsensitiveItemSorter());
 		entries.sort(propertyId, ascending);
 		return entries;
 	}
@@ -100,5 +105,56 @@ public class BPTEntryCards extends BPTShowEntryComponent {
 		_id = entryId;
 		addConfirmationWindow(null, status);
 	}
+	
+	/**
+     * Constructs a CaseInsensitiveItemSorter that uses a case-insensitive sorter for string property values,
+     * and the default otherwise.
+     *
+     */
+	class CaseInsensitiveItemSorter extends DefaultItemSorter {
 
+	    public CaseInsensitiveItemSorter() {
+	        super(new CaseInsensitivePropertyValueComparator());
+	    }
+
+	}
+	
+	/**
+     * Provides a case-insensitive comparator used for comparing string {@link Property} values.
+     * The <code>CaseInsensitivePropertyValueComparator</code> assumes all objects it
+     * compares can be cast to Comparable.
+     *
+     */
+	class CaseInsensitivePropertyValueComparator implements Comparator<Object>, Serializable {
+
+        @SuppressWarnings("unchecked")
+        public int compare(Object o1, Object o2) {
+        	
+            int r = 0;
+        	Collator comparator = Collator.getInstance();
+    		comparator.setStrength(Collator.PRIMARY);
+            // Normal non-null comparison
+            if (o1 != null && o2 != null) {
+                if ((o1 instanceof String) && (o2 instanceof String)) {
+                	return comparator.compare(o1, o2);
+                    
+                } else {
+                    // Assume the objects can be cast to Comparable, throw
+                    // ClassCastException otherwise.
+                    r = ((Comparable<Object>) o1).compareTo(o2);
+                }
+            } else if (o1 == o2) {
+                // Objects are equal if both are null
+                r = 0;
+            } else {
+                if (o1 == null) {
+                    r = -1; // null is less than non-null
+                } else {
+                    r = 1; // non-null is greater than null
+                }
+            }
+
+            return r;
+        }
+    }
 }
