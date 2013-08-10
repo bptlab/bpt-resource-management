933d300c4501b67f7799f51d79e68aff0190ec5c
livetsunni <livetsunni@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2013-08-10 11:33:57 +0000

    bpmai: proper display & storage of attachments via BPTUploader in CouchDB

---
+++ branches/bpmai/bpt-resource-management-common/src/main/java/de/uni_potsdam/hpi/bpt/resource_management/ektorp/BPTMimeTypes.java
@@ -0,0 +1,36 @@
+package de.uni_potsdam.hpi.bpt.resource_management.ektorp;
+
+/**
+ * Enum for supported MIME types of documents for BPMAI
+ * 
+ * @author tw
+ *
+ */
+public enum BPTMimeTypes {
+	PDF ("application/pdf"),
+	DOC ("application/msword"),
+	DOCX ("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+	
+	private final String mimeType;
+	
+	BPTMimeTypes(String mimeType) {
+        this.mimeType = mimeType;
+    }
+	
+	/**
+	 * 
+	 * @return supported MIME types of documents for BPMAI
+	 */
+	public static String[] getMimeTypes() {
+		String[] mimeTypes = new String[values().length];
+		for (int i = 0; i < values().length; i++) {
+			mimeTypes[i] = values()[i].toString();
+		}
+		return mimeTypes;
+	}
+	
+	@Override
+	public String toString() {
+		return mimeType;
+	}
+}
