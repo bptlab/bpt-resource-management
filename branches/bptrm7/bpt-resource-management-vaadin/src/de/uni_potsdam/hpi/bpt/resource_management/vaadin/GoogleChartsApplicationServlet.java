0ca5ff6832a5a7481c9bec02391aba1408dc0968
livetsunni <livetsunni@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2013-11-07 16:56:10 +0000

    OpenID (not that dirty) (contd.)

--- branches/bptrm7/bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/GoogleChartsApplicationServlet.java
+++ branches/bptrm7/bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/GoogleChartsApplicationServlet.java
@@ -1,16 +1,38 @@
 package de.uni_potsdam.hpi.bpt.resource_management.vaadin;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
+import com.vaadin.server.DeploymentConfiguration;
+import com.vaadin.server.RequestHandler;
+import com.vaadin.server.ServiceException;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.server.VaadinServletService;
+
+import de.uni_potsdam.hpi.bpt.resource_management.vaadin.utils.PageRefreshRequestHandler;
 
 public class GoogleChartsApplicationServlet extends VaadinServlet {
 
-//    protected void writeAjaxPageHtmlHeader(final BufferedWriter page, String title, String themeUri, HttpServletRequest request) throws IOException {
+	private static final long serialVersionUID = 9140715679884065420L;
+
+	//    protected void writeAjaxPageHtmlHeader(final BufferedWriter page, String title, String themeUri, HttpServletRequest request) throws IOException {
 //        page.write("<script type=\"text/javascript\" src=\"googleCharts.js\"></script>");
 //        super.writeAjaxPageHtmlHeader(page, title, themeUri, request);
 //   }
+    @Override
+    
+    protected VaadinServletService createServletService(DeploymentConfiguration deploymentConfiguration) throws ServiceException {
+        VaadinServletService service = new VaadinServletService(this, deploymentConfiguration) {
+			private static final long serialVersionUID = 4002353845450193504L;
+			@Override
+            protected List<RequestHandler> createRequestHandlers() throws ServiceException {
+                List<RequestHandler> handlers = super.createRequestHandlers();
+                // adds request handler at the beginning of list
+                // because VaadinService reverses this list
+                handlers.add(new PageRefreshRequestHandler());
+                return handlers;
+            }
+        };
+        service.init();
+        return service;
+    }
 }
