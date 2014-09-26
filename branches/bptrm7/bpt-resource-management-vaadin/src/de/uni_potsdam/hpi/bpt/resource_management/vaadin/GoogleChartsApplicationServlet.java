b7b7708e008ca850e66807200010c5484052d56b
livetsunni <livetsunni@4abd21b1-86af-ae7a-fbd3-c2305b9f6a54>
2014-09-26 10:05:51 +0000

    bptrm7: integrated Piwik script snippet

--- branches/bptrm7/bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/GoogleChartsApplicationServlet.java
+++ branches/bptrm7/bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/GoogleChartsApplicationServlet.java
@@ -2,9 +2,16 @@ package de.uni_potsdam.hpi.bpt.resource_management.vaadin;
 
 import java.util.List;
 
+import javax.servlet.ServletException;
+
+import com.vaadin.server.BootstrapFragmentResponse;
+import com.vaadin.server.BootstrapListener;
+import com.vaadin.server.BootstrapPageResponse;
 import com.vaadin.server.DeploymentConfiguration;
 import com.vaadin.server.RequestHandler;
 import com.vaadin.server.ServiceException;
+import com.vaadin.server.SessionInitEvent;
+import com.vaadin.server.SessionInitListener;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.server.VaadinServletService;
 
@@ -33,5 +40,36 @@ public class GoogleChartsApplicationServlet extends VaadinServlet {
         };
         service.init();
         return service;
+    }
+    
+    @Override
+    protected void servletInitialized() throws ServletException {
+        super.servletInitialized();
+        getService().addSessionInitListener(new SessionInitListener() {
+			private static final long serialVersionUID = 5057367721880923712L;
+			@Override
+            public void sessionInit(SessionInitEvent event) {
+                event.getSession().addBootstrapListener(new BootstrapListener() {
+					private static final long serialVersionUID = 5651095681114698532L;
+					@Override
+                    public void modifyBootstrapPage(BootstrapPageResponse response) {
+                         response.getDocument().head().append("<script  type=\"text/javascript\" src=\"./VAADIN/themes/bpt/piwik.js\"/>");
+                         response.getDocument().body().append("<noscript><p><img src=\"http://bpt.hpi.uni-potsdam.de/piwik/piwik.php?idsite=2\" style=\"border:0;\" alt=\"\" /></p></noscript>");
+                    }
+                    @Override
+                    public void modifyBootstrapFragment(BootstrapFragmentResponse response) {
+//                        // Wrap the fragment in a custom div element
+//                        Element myDiv = new Element(Tag.valueOf("div"), "");
+//                        List<Node> nodes = response.getFragmentNodes();
+//                        for(Node node : nodes) {
+//                            myDiv.appendChild(node);
+//                        }
+//                        nodes.clear();
+//                        nodes.add(myDiv);
+                    }
+                }
+        );
+            }
+        });
     }
 }
