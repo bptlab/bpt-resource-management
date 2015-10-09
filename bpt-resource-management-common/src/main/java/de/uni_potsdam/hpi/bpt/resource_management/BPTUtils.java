e761f4f65e03bdb1fca203e32b97e741086fd786
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2015-10-09 08:45:25 +0200

    temp folder for images moved to user home

--- bpt-resource-management-common/src/main/java/de/uni_potsdam/hpi/bpt/resource_management/BPTUtils.java
+++ bpt-resource-management-common/src/main/java/de/uni_potsdam/hpi/bpt/resource_management/BPTUtils.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class BPTUtils {
 
 	public static String getTempFolder() {
-		return File.separator + "usr" + File.separator + "share" + File.separator + "tomcat7" + File.separator + "webapps" + File.separator + "temp";
+		return System.getProperty("user.home") + File.separator + "bpmToolsTemp";
 //		return "C:" + File.separator + "temp";
 	}
 }
