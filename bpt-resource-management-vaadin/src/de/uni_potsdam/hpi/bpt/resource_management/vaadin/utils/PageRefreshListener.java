3e8d9fc74a10ab00cba308f4114f1195b3e18b2e
Marcin Hewelt <hewelt@hpi.uni-potsdam.de>
2015-10-15 18:20:00 +0200

    removed folder bpt-resource-management-vaadin, to be replaced with maven project

--- bpt-resource-management-vaadin/src/de/uni_potsdam/hpi/bpt/resource_management/vaadin/utils/PageRefreshListener.java
+++
@@ -1,9 +0,0 @@
-package de.uni_potsdam.hpi.bpt.resource_management.vaadin.utils;
-
-import com.vaadin.server.VaadinRequest;
-
-public interface PageRefreshListener {
-	
-	void pageRefreshed(VaadinRequest request);
-
-}
